a03afd25d4e603c6e7a7e47c1b257a8448b2bb52
innoventix-solutions <Innoventixgithub@gmail.com>
2024-03-26 18:21:51 +0530

    Changes: -- All Done

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'News 12 Paper',
+      title: 'News 12',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
